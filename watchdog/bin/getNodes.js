cad0957bcc58bac2e359ed4ee7842a888c9a9efd
ajaxabernathy <ajax.abernathy@gmail.com>
2023-09-27 12:34:43 -0400

    start comments and cleanup

--- watchdog/bin/getNodes.js
+++ watchdog/bin/getNodes.js
@@ -1,17 +1,21 @@
 const k8s = require('@kubernetes/client-node');
 
+// DISPLAY ALL CURRENT NODES
+
 module.exports = () => {
-    const kc = new k8s.KubeConfig();
-    kc.loadFromDefault();
-    const k8sApi = kc.makeApiClient(k8s.CoreV1Api); 
-    k8sApi.listNode().then((res) => {
+  const kc = new k8s.KubeConfig();
+  kc.loadFromDefault();
+  const k8sApi = kc.makeApiClient(k8s.CoreV1Api);
+  k8sApi
+    .listNode()
+    .then((res) => {
       console.log('Nodes:'.cyan);
       res.body.items.forEach((node) => {
-          console.log(`   ${node.metadata.name}`);
+        console.log(`   ${node.metadata.name}`);
       });
       process.exit();
-  })
-  .catch((err) => {
+    })
+    .catch((err) => {
       console.error('Error:', err);
-  });
-  };
+    });
+};
